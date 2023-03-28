2e160acb633470fbfde3a831c2c719004b32e221
thecoder714 <75258277+thecoder714@users.noreply.github.com>
2023-03-28 11:37:41 +0530

    fixed it

--- laptop.js
+++ laptop.js
@@ -2,20 +2,15 @@ var img = "";
 objects = [];
 var level = false;
 
-function preload() {img = loadImage('laptop.jpg');}
+function preload() {img = loadImage('laptop.webp');}
 
 function setup() {
-    canvas = createCanvas(380, 380);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.size(380, 380);
-    video.hide();
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
-  }
+}
   
 function modelLoaded() {
-    console.log("Model Loaded!")
+    console.log("Model Loaded!");
     level = true;
 }
   
@@ -26,24 +21,22 @@ function gotResult(error, results) {
 }
 
 function draw() {
-    image(video, 0, 0, 380, 380);
-        if (level != false) {
+    image(img, 0, 0, 380, 380);
+        if (level = true) {
             r = random(255);
             g = random(255);
             b = random(255);
-            objectDetector.detect(video, gotResult);
+            objectDetector.detect(img, gotResult);
+            //document.getElementById("status").innerHTML = "Status: Objects Detected";
+            //document.getElementById("numberofobjects").innerHTML = objects.length;
 
-            for (let i = 0; i < objects.length; i++) {
-                document.getElementById("status").innerHTML = "Status: Objects Detected";
-                document.getElementById("numberofobjects").innerHTML = objects.length;
-                // fill(r, g, b);
-                fill(0, 0, 0)
+            for (let i = 0; i <= objects.length; i++) {
                 percent = floor(objects[i].confidence * 100);
-                text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
-                noFill();
-                // stroke(r, g, b);
-                rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+                console.log("percent: " + percent);
+                document.getElementById("object").innerHTML += objects[i].label +  ": " + percent + "%" + " ";
             }
+            noLoop();
         }
 }
 
+setTimeout(function() {draw()}, 2000);
